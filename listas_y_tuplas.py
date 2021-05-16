732800778f19ea78d2b23184370bb1e5426b1e92
Hayen Calderon <hayen100001@gmail.com>
2021-05-16 02:26:20 -0600

    Archivo listas_y_tuplas

---
+++ listas_y_tuplas.py
@@ -0,0 +1,17 @@
+def run():
+    
+    # Las tuplas van con ()
+    # Son inmutables
+    # No se le pueden agregar ni quitar valores
+    
+    mi_tupla = (1, 2, 3, 4, 5, 6, "Hola")
+    print(mi_tupla)
+    
+    for i in mi_tupla:
+        print(i)
+
+
+
+
+if __name__ == '__main__':
+    run()
