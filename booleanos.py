585e9f716f08250adbdabeeef63835dfb8ab7b88
Hayen Calderon <hayen100001@gmail.com>
2021-05-15 20:19:28 -0600

    Haciendo un par de ejemplos de booleanos

---
+++ booleanos.py
@@ -0,0 +1,19 @@
+def run():
+     
+    print("¿7 es igual a 7, eso es True o False?")
+    numero1 = 7
+    numero2 = 7
+    resultado1 = numero1 == numero2
+    print(resultado1)
+
+def run2():
+    print("¿7 es diferente a 7, eso es True o False?")
+    numero1 = 7
+    numero2 = 7
+    resultado1 = numero1 != numero2
+    print(resultado1)
+
+if __name__ == '__main__':
+    run()
+    run2()
+
