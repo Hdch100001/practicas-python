54776dc793927e7e869b9f7f321f7d1b366d5c75
Hayen Calderon <hayen100001@gmail.com>
2021-05-15 20:35:26 -0600

    Actualización de conversiones de monedas

--- conversor_inicial.py
+++ conversor_inicial.py
@@ -1,7 +1,7 @@
-pesos = input("¿cuantos pesos colombianos tienes?: ")
+pesos = input("¿cuantos pesos costarricenses tienes?: ")
 pesos = float(pesos)
-valor_dolar = 3875
+valor_dolar = 619
 dolares = pesos / valor_dolar 
 dolares = round(dolares, 2)
 dolares = str(dolares)
-print ("Tienes $" + dolares + "dólares")
+print ("Tienes $" + dolares + " dólares")
