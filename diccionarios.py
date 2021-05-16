0f40da4a6a8446d8a100926a2b2626680e9e785a
Hayen Calderon <hayen100001@gmail.com>
2021-05-16 02:36:41 -0600

    Diccionarios con python

--- diccionarios.py
+++ diccionarios.py
@@ -1,10 +1,10 @@
 def run():
-    # mi_diccionario = {
-    #     'llave1': 1,
-    #     'llave2': 2,
-    #     'llave3': 3,
+    mi_diccionario = {
+        'llave1': 1,
+        'llave2': 2,
+        'llave3': 3,
+    }
 
-    # }
     # print(mi_diccionario['llave1'])
     # print(mi_diccionario['llave2'])
     # print(mi_diccionario['llave3'])
@@ -19,6 +19,10 @@ def run():
 
     # for pais in poblacion_paises.keys():
     #     print(pais)
+
+    # for pais in poblacion_paises.values():
+    #     print(pais)
+
     for pais, poblacion in poblacion_paises.items():
         print(pais  +  ' tiene '  +  str(poblacion)  +  ' habitantes ' )
 
