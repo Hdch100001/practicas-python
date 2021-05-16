9309d875dcf5803b2081431af5a6bbd1320fede0
Hayen Calderon <hayen100001@gmail.com>
2021-05-16 02:17:09 -0600

    Aprendiendo diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -1,10 +1,10 @@
 def run():
-    mi_diccionario = {
-        'llave1': 1,
-        'llave2': 2,
-        'llave3': 3,
+    # mi_diccionario = {
+    #     'llave1': 1,
+    #     'llave2': 2,
+    #     'llave3': 3,
 
-    }
+    # }
     # print(mi_diccionario['llave1'])
     # print(mi_diccionario['llave2'])
     # print(mi_diccionario['llave3'])
