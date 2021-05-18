55463da3de7e72daab36f81d49bef6a746d95f0f
Hayen Calderon <hayen100001@gmail.com>
2021-05-17 19:10:56 -0600

    Usando ciclo for para anidados de listas y disccionarios

--- list_and_dicts.py
+++ list_and_dicts.py
@@ -1,3 +1,15 @@
+# MENSAJE:
+# Se puede crear listas dentro de diccionarios y viceversa diccionarios dentro de listas
+# Los diccionarios dentro de listas pueden ir dentro de dos corchetes que identifican la lista, y las llaves de los diccionarios 
+# para dentro de esas llaves identificar nuestra llave entre comillas y luego : para nuevamente entre comillas el valor de esa llave. 
+# Dentro de cada llave de diccionario pueden ir varios valores con sus respectivas llaves separadas por dos puntos.
+
+# Cada vez que se use for para estos aninados debemos tener en cuenta eh identificar bien llaves y valores
+# A la hora de usar for en listas con diccionarios aninados tenemos muchas maneras de imprimir los objetos dentro de ellas
+
+
+
+
 def run():
     my_list = [1, "Hello", True, 4.5]
     my_dict = {"firstname": "Facundo", "last": "García"}
--- list_and_dicts2.py
+++ list_and_dicts2.py
@@ -1,3 +1,13 @@
+# MENSAJE:
+# Se puede crear listas dentro de diccionarios y viceversa diccionarios dentro de listas
+# Los diccionarios dentro de listas pueden ir dentro de dos corchetes que identifican la lista, y las llaves de los diccionarios 
+# para dentro de esas llaves identificar nuestra llave entre comillas y luego : para nuevamente entre comillas el valor de esa llave. 
+# Dentro de cada llave de diccionario pueden ir varios valores con sus respectivas llaves separadas por dos puntos.
+
+# Cada vez que se use for para estos aninados debemos tener en cuenta eh identificar bien llaves y valores
+# A la hora de usar for en listas con diccionarios aninados tenemos muchas maneras de imprimir los objetos dentro de ellas
+
+
 def run():
     # my_list = [1, "Hello", True, 4.5]
     # my_dict = {"firstname": "Facundo", "last": "García"}
@@ -20,8 +30,11 @@ def run():
     #     for key, values in i.items():
     #         print(key, ": ",values)
 
+    # for i in super_list:
+    #     print(i.items())
+
     for i in super_list:
-        print(i.items())
+        print(i['firstname'],i['lastname'])
 
 
 
