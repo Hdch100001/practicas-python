9cb9bc8f3b91f7c028d8503bc9e6724ae01f8f70
Hayen Calderon <hayen100001@gmail.com>
2021-05-18 14:56:15 -0600

    Dare dictionary comprehensions

---
+++ dare_dicts_comprehensions.py
@@ -0,0 +1,24 @@
+# Reto clase 10
+#       Crear un dictc comprehensions con los numeros del 1 al 1000 como llaves y como valores
+#     cada raiz cuadrada de la llave solo si la llave es divisible entre 9.
+
+
+
+def run():
+    
+    # my_dict = {}
+
+    # for i in range(1, 1001):
+    #     if i % 9 == 0:
+    #         my_dict[i] = i**(1/2)
+
+    my_dict = {i: i**(1/2) for i in range(1,1001) if i % 9 ==0}
+    
+    print(my_dict)
+
+
+
+
+if __name__ == '__main__':
+    run()
+
--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -8,20 +8,22 @@
 #                         {key: value for value in iterable if condition}
                             
                         
-
-
-
 def run():
-    dictionary = {}
+#     dictionary = {}
 
-    for i in range(1, 101):
-        if i % 3 != 0:
-            dictionary[i] = i**3 
-
-    print(dictionary)
+#     for i in range(1, 101):
+#         if i % 3 != 0:
+#             dictionary[i] = i**3 
 
 
+    my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+
+
+    print(my_dict)
 
 
 if __name__ == '__main__':
     run()
+
+
+
