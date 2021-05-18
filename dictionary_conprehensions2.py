4a19d1d9c93e671a41c9633c7c0f8f228200706d
Hayen Calderon <hayen100001@gmail.com>
2021-05-18 14:08:52 -0600

    Elaborando dictionary comprehensions

---
+++ dictionary_conprehensions2.py
@@ -0,0 +1,27 @@
+# Clase 10
+#     Tema: Dictionary comprehensions
+
+#         Creaando un diccionario cuyas llaves tinen guardada una operacion
+#         Luego ejecutamos un ciclo for, en este caso function rage 
+#         Esto con la estructura siguiente:
+
+#                         {key: value for value in iterable if condition}
+                            
+                        
+
+
+
+def run():
+    dictionary = {}
+
+    for i in range(1, 101):
+        if i % 3 != 0:
+            dictionary[i] = i**3 
+
+    print(dictionary)
+
+
+
+
+if __name__ == '__main__':
+    run()
